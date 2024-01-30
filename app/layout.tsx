c0b10f4e704664ea41c57b003ad4ca550dfa87a4
Rendy Pratama <rendypratama752@gmail.com>
2024-01-30 19:38:30 +0700

    ADD: header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
+import Header from '@/components/header';
+import ThemeContextProvider from '@/context/theme.context';
+import ActiveSectionContextProvider from '@/context/active-section-context';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -19,7 +22,12 @@ export default function RootLayout({
       <body className={`${inter.className} bg-gray-50 text-gray-950 relative`}>
         <div className="bg-violet-100 absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
         <div className="bg-blue-200 absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
-        {children}
+        <ThemeContextProvider>
+          <ActiveSectionContextProvider>
+            <Header />
+            {children}
+          </ActiveSectionContextProvider>
+        </ThemeContextProvider>
       </body>
     </html>
   );
