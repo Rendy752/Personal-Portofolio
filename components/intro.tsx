7a8c11f75b943c08baa2b6b2d73bd5a8b1e7bf67
Rendy Pratama <rendypratama752@gmail.com>
2024-02-03 08:39:08 +0700

    FIX: header smooth

--- components/intro.tsx
+++ components/intro.tsx
@@ -15,7 +15,7 @@ export default function Intro() {
     <section
       ref={ref}
       id="home"
-      className="mb-28 max-w-[50rem] text-center sm:mb-0"
+      className="mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]"
     >
       <div className="flex items-center justify-center">
         <div className="relative">
